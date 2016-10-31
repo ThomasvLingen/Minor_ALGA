32489490a177ed2dc08d1ae799bf6b871f902d29
jorgdebont <jacbont@student.avans.nl>
2016-10-31 13:26:32 +0100

    feat: started on floor, created print and are_adjecent function

--- src/Floor.cpp
+++ src/Floor.cpp
@@ -3,3 +3,59 @@
 //
 
 #include "Floor.hpp"
+
+
+Floor::Floor(int width, int height)
+: width(width)
+, height(height)
+{
+    this->rooms.resize((size_t)(width * height)); //all nullptrs now
+    rooms[0] = new Room(RoomType::START, 0);
+    for(int index = 1; index < (width * height - 1); index++){
+        rooms[index] = new Room(RoomType::NORMAL, 0);
+    }
+    rooms[height*width - 1] = new Room(RoomType::END, 0);
+}
+
+Floor::~Floor()
+{
+
+}
+
+void Floor::print_map()
+{
+    for(int index = 0; index < (int)this->rooms.size(); index++){
+        if(rooms[index] == nullptr){
+            cout << '.';
+        } else {
+            cout << room_type_to_char_map[rooms[index]->room_type];
+        }
+
+        if((index + 1) % width == 0){
+            cout << endl;
+        } else {
+            cout << "  "; //adjen
+        }
+    }
+}
+
+void Floor::build_adjecency_list()
+{
+    for(int index = 0; index < (int)rooms.size(); index++){
+
+    }
+}
+
+bool Floor::are_adjecent(int room_1_index, int room_2_index)
+{
+    if(room_2_index == room_1_index + width || room_2_index == room_1_index - width){
+        return true;
+    }
+    else if(room_1_index + 1 == room_2_index && room_2_index % width != 0){
+        return true;
+    }
+    else if (room_1_index - 1 == room_2_index && (room_2_index + 1) % width != 0) {
+        return true;
+    }
+    return false;
+}
--- src/Floor.hpp
+++ src/Floor.hpp
@@ -6,8 +6,37 @@
 #define SRC_FLOOR_HPP
 
 
-class Floor {
+#include "Room.hpp"
+#include <vector>
+#include <iostream>
+#include <map>
+#include "RoomEdge.hpp"
 
+using std::vector;
+using std::cout;
+using std::endl;
+using std::map;
+
+class Floor {
+public:
+    Floor(int width, int height);
+    virtual ~Floor();
+
+    int width;
+    int height;
+    void print_map();
+    void build_adjecency_list();
+    bool are_adjecent(int room_1_index, int room_2_index);
+
+    vector<Room*> rooms;
+
+    map<int, RoomEdge> adjecency_list;
+
+    map<RoomType, char> room_type_to_char_map = {
+        {RoomType::NORMAL, 'n'},
+        {RoomType::START, 'S'},
+        {RoomType::END, 'E'}
+    };
 };
 
 
