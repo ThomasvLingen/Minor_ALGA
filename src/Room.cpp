0326f57b9178098f68e34f3fc5a30f8360c0ef50
ThomasvLingen <thovali96@gmail.com>
2016-10-31 13:04:51 +0100

    fix: fix initialiser list order

--- src/Room.cpp
+++ src/Room.cpp
@@ -5,8 +5,9 @@
 #include "Room.hpp"
 
 Room::Room(RoomType room_type, int enemy_level)
-: enemy_level(enemy_level)
-, room_type(room_type)
+: room_type(room_type)
+, enemy_level(enemy_level)
+
 {
 
 }
